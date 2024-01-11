e4b5cb7527c332f5e131a36e19f5a516d60f5ad2
Michał Gruszczyński <123127384+MisiekT3@users.noreply.github.com>
2024-01-11 16:39:05 +0100

    Update App.tsx
    Updejcik
    
    Wprowadziłem plików bazy danych

--- App.tsx
+++ App.tsx
@@ -2,9 +2,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import HomeScreen from './HomeScreen'; // Importuje ekran domowy
-// import SearchScreen from './SearchScreen' // Importuje ekran szukania produktu
-// import CartScreen from './CartScreen'; // Importuje ekran domowy
+// import HomeScreen from './HomeScreen'; // Importuje ekran domowy
+import DatabaseScreen from "./Screens/DatabaseScreen";
 import ProfileNavigation from './ProfileNavigation'; // Importuje nawigacje profilową z jej ekranami
 
 const Tab = createMaterialBottomTabNavigator();
@@ -13,9 +12,8 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        {/*<Tab.Screen name="Search" component={SearchScreen} />*/}
-        {/*<Tab.Screen name="Cart" component={CartScreen} />*/}
+        <Tab.Screen name="Database" component={DatabaseScreen} />
+        {/*<Tab.Screen name="Home" component={HomeScreen} />*/}
         <Tab.Screen name="Profile" component={ProfileNavigation} />
       </Tab.Navigator>
     </NavigationContainer>
